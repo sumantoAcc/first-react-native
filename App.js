e6ceec9d0d556c954d08c22a7baef06b3b6312a1
sumanto pal <sumanto.pal@accolitedigital.com>
2021-02-23 20:56:11 +0530

    ss

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, FlatList } from 'react-native';
+import Dashboard from "./components/Dashboard"
+import NewsDesc from "./components/NewsDesc"
+import Login from "./components/Login"
 
 export default function App() {
+  const [items, setItems] = useState([
+    {
+      id: Math.floor((Math.random() * 1000)).toString(),
+      text: 'Milk'
+    },
+    {
+      id: Math.floor((Math.random() * 1000)).toString(),
+      text: 'Chocolate'
+    },
+    {
+      id: Math.floor((Math.random() * 1000)).toString(),
+      text: 'Tissue Paper'
+    },
+    {
+      id: Math.floor((Math.random() * 1000)).toString(),
+      text: 'Toothbrush'
+    },
+  ]);
+
+  const deleteItem = (id) => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  }
+  const addItem = (text) => {
+    setItems(prevItems => {
+      return [{
+        id: Math.floor((Math.random() * 1000)).toString(),
+        text: text
+      },
+      ...prevItems];
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header title="Shopping List" />
+      <AddItem addItem={addItem}/>
+      <FlatList
+        data={items}
+        renderItem={({ item }) =>
+          <ListItem item={item}
+            deleteItem={deleteItem} />
+        }
+      />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 60
   },
 });
